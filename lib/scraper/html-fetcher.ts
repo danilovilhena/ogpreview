66f79448957e9693d40fdb7e8b5e8e9e2f5e8820
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2025-09-25 17:01:39 -0300

    feat: ua

--- lib/scraper/html-fetcher.ts
+++ lib/scraper/html-fetcher.ts
@@ -56,6 +56,7 @@ const USER_AGENTS = [
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0',
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15',
   'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+  'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
 ];
 
 function getRandomUserAgent(): string {
